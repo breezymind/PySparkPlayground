6d9f112fd6802e7c2e93160be2cd41c0f0a929df
Dirk Dupont <dirk@questra-consulting.be>
2018-01-16 11:32:48 +0100

    Added a few additional clarifications.

--- scaling_data.py
+++ scaling_data.py
@@ -1,6 +1,13 @@
 """
 Example of how to standardizes features by removing the mean and scaling to 
 unit variance.
+
+RBF kernel of Support Vector Machines or the L1 and L2 regularized linear models
+typically work better when all features have unit variance and/or zero mean.
+
+Standardization can improve the convergence rate during the optimization
+process, and also prevents against features with very large variances exerting
+an overly large influence during model training.
 """
 
 from pyspark.ml.feature import StandardScaler
@@ -13,6 +20,8 @@ if __name__ == "__main__":
         .getOrCreate()
 
     dataFrame = spark.read.format("libsvm").load("data/sample_libsvm_data.txt")
+
+    # Standardizing features by scaling to unit variance
     scaler = StandardScaler(inputCol="features", outputCol="scaledFeatures",
                             withStd=True, withMean=False)
 
@@ -21,9 +30,9 @@ if __name__ == "__main__":
 
     # Normalize each feature to have unit standard deviation.
     scaledData = scalerModel.transform(dataFrame)
-    scaledData.show()
+    scaledData.show(5)
 
-    # This time withMean = True
+    # Standardizing features by scaling to unit variance and removing the mean
     scaler = StandardScaler(inputCol="features", outputCol="scaledFeatures",
                             withStd=True, withMean=True)
 
@@ -32,6 +41,6 @@ if __name__ == "__main__":
 
     # Normalize each feature to have unit standard deviation.
     scaledData = scalerModel.transform(dataFrame)
-    scaledData.show()
+    scaledData.show(5)
 
     spark.stop()

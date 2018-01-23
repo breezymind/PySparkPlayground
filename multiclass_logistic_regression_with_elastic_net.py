faf4339bfe257516f237510856dc33a0905313fb
Dirk Dupont <dirk@questra-consulting.be>
2018-01-23 20:31:00 +0100

    Added more documentation to multiclass logistic regression example

--- multiclass_logistic_regression_with_elastic_net.py
+++ multiclass_logistic_regression_with_elastic_net.py
@@ -1,27 +1,39 @@
 """
-An example demonstrating Multiclass Logistic Regression with ElasticNet.
+Multiclass Logistic Regression With Elastic Net.
 
+Multiclass classification is supported via multinomial logistic (softmax) 
+regression. In multinomial logistic regression, the algorithm produces K sets 
+of coefficients, or a matrix of dimension K×J where K is the number of 
+outcome classes and J is the number of features. If the algorithm is fit with 
+an intercept term then a length K vector of intercepts is available.
+
+Multinomial coefficients are available as coefficientMatrix and intercepts are 
+available as interceptVector.
+
+coefficients and intercept methods on a logistic regression model trained with 
+multinomial family are not supported. Use coefficientMatrix and interceptVector 
+instead.
 """
 
 from pyspark.ml.classification import LogisticRegression
 from pyspark.sql import SparkSession
 
-
 if __name__ == "__main__":
-    spark = SparkSession\
-        .builder\
-        .appName("MulticlassLogisticRegressionWithElasticNet")\
+    spark = SparkSession \
+        .builder \
+        .appName("MulticlassLogisticRegressionWithElasticNet") \
         .getOrCreate()
 
-    # Loads data.
-    dataset = spark.read.format("libsvm")\
+    # Load training data
+    training = spark \
+        .read \
+        .format("libsvm") \
         .load("data/sample_multiclass_classification_data.txt")
-    dataset.show()
 
-    lr = LogisticRegression(maxIter=3, regParam=0.3, elasticNetParam=0.8)
+    lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
 
     # Fit the model
-    lrModel = lr.fit(dataset)
+    lrModel = lr.fit(training)
 
     # Print the coefficients and intercept for multinomial logistic regression
     print("Coefficients: \n" + str(lrModel.coefficientMatrix))
